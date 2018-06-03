6db5727bf01ca002688089350b3cb42838fa9150
Nerova <OO3803@gmail.com>
2018-06-03 16:30:51 +0800

    ReadFile

--- main.asm
+++ main.asm
@@ -1,17 +1,37 @@
-; AddTwo.asm - adds two 32-bit integers.
-; Chapter 3 example
 INCLUDE Irvine32.inc
+
 .386
 .model flat,stdcall
 .stack 4096
-ExitProcess proto,dwExitCode:dword
+ExitProcess PROTO, dwExitCode: DWORD
+
+.data
+MAX = 4096
+inputHandle HANDLE ?
+fileName BYTE 80 DUP(?)
+buffer BYTE MAX DUP(?)
+bytesRead DWORD ?
 
 .code
 main proc
-	mov	eax,5				
-	add	eax,6				
+	mov edx, OFFSET fileName
+	mov ecx, SIZEOF fileName
+	call ReadString
 
-     call WaitMsg
+	mov edx, OFFSET fileName
+	call OpenInputFile
+	mov inputHandle, eax
+	
+	mov eax, inputHandle
+	mov edx, OFFSET buffer
+	mov ecx, SIZEOF buffer
+	call ReadFromFile
+	mov bytesRead, eax
+
+	mov edx, OFFSET buffer
+	call WriteString
+
+    call WaitMsg
 	invoke ExitProcess,0
 main endp
 end main
