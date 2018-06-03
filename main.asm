f2d1185546315b9e25fcc8593a5d0b4fc5258d65
fdmdkw <lk1919618@gmail.com>
2018-06-03 16:14:49 +0800

    include Irvine32.inc

--- main.asm
+++ main.asm
@@ -1,6 +1,6 @@
 ; AddTwo.asm - adds two 32-bit integers.
 ; Chapter 3 example
-
+INCLUDE Irvine32.inc
 .386
 .model flat,stdcall
 .stack 4096
@@ -11,6 +11,7 @@ main proc
 	mov	eax,5				
 	add	eax,6				
 
+     call WaitMsg
 	invoke ExitProcess,0
 main endp
 end main
